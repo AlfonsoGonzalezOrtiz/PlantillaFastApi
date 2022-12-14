d8227f37dcdbcfdb5b43f840df22b70c4ea46845
Alfonso Gonzalez <alfonsogonzalezortiz@uma.es>
2022-12-14 23:47:24 +0100

    Lista de fotos

--- models/model.py
+++ models/model.py
@@ -1,10 +1,10 @@
-from typing import Optional
+from typing import Optional,List
 from pydantic import BaseModel
 
 class exam(BaseModel):
     email: str
     num: int
-    url: str
+    url: List[str]
 
 class examUpdate(BaseModel):
     email: Optional[str]
