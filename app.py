36a902fbac97e27123d36d2c05c0bb11fc37e985
Alfonso Gonzalez <alfonsogonzalezortiz@uma.es>
2022-12-14 22:51:38 +0100

    Middleware added

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from fastapi.responses import RedirectResponse
-
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, Request
 from routes.exam import routerExam
 from routes.test import routerTest
@@ -8,6 +8,18 @@ app = FastAPI(
     title="REST API with FastAPI and MongoDB",
     description="Example of description",
     version="0.1.0"
+)
+
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 @app.get("/", include_in_schema=False)
